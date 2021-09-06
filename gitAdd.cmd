55e1b10e173e433f8fcab06522312b9b37bb3d56
DanielAdeniji <daniel_adeniji@hotmail.com>
2021-09-06 14:13:35 -0700

    Adding new file gitLog.summary.cmd

---
+++ gitAdd.cmd
@@ -0,0 +1,15 @@
+git add sample.dtd.internal.xml
+
+git add sample.dtd.external.xml
+
+git add sample.dtd.external.element.added.finance.xml
+
+git add sample.dtd.external.element.added.finance.commented.simple.xml
+
+git add sample.dtd.external.element.added.finance.commented.nested.xml
+
+git add sample.dtd.external.element.added.finance.commented.unusedProcessingInstruction.xml
+
+git add README.md
+
+git add git*.cmd
